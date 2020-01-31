93da1caf56e329b9689a30049d24b1f5fac7bc61
LiBrito <lizbethbritou@gmail.com>
2020-01-31 01:36:28 -0400

    creacion del login

---
+++ EfCodeFirst/EfCodeFirst/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EfCodeFirst.Models;
+
+namespace EfCodeFirst.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            if (acc.User == "Admin" && acc.Password == "SoyTuRealAdministrador2020")
+            {
+                return RedirectToAction("Index", "BlogPosts");
+            }
+            else
+            {
+                ViewBag.Message = "User o Passwaord erroneas";              
+                return RedirectToAction("Login", "Account");
+            }
+
+        }
+    }
+}
---
+++ EfCodeFirst/EfCodeFirst/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EfCodeFirst.Models
+{
+    public class Account
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
